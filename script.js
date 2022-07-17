75b1c4085be98a0ffb04091bc6bba3c2b9827e93
MoSaleh20 <16m.saleh@gmail.com>
2022-07-17 04:02:16 +0300

    added more features

--- script.js
+++ script.js
@@ -1,85 +1,199 @@
-const save = document.getElementById('save');
-const clear = document.getElementById('clear');
-const attended = document.getElementsByClassName('attendedButton');
-const absent = document.getElementsByClassName('absentButton');
-const late = document.getElementsByClassName('lateButton');
-const attendedCount = document.getElementById('attendedCount');
-const absentCount = document.getElementById('absentCount');
-const lateCount = document.getElementById('lateCount');
+const saveButton = document.getElementById('save');
+const clearButton = document.getElementById('clear');
+const attendedButton = document.getElementsByClassName('attendedButton');
+const absentButton = document.getElementsByClassName('absentButton');
+const lateButton = document.getElementsByClassName('lateButton');
+const attendedButtonCount = document.getElementById('attendedCount');
+const absentButtonCount = document.getElementById('absentCount');
+const lateButtonCount = document.getElementById('lateCount');
+const addForm = document.getElementById('addForm');
+const table = document.getElementById("attendTableBody");
+const popupDialog = document.getElementById('pop-up');
+const cancelButton = document.getElementById('cancel');
+const addStudentButton = document.getElementById('addStudent');
+const title = document.getElementById('title');
 
 
-//attendance button
+// add current date to title
+var date = new Date()
+title.innerHTML+= date.getFullYear()+'/'+Number(Number(date.getMonth())+1)+'/'+date.getDate();
+
+
+//attendance button (for each student)
 function att(){
-   for(var i = 0; i<attended.length;i++){
-    attended[i].addEventListener('click', function(e){
+   for(var i = 0; i<attendedButton.length;i++){
+    attendedButton[i].addEventListener('mouseover', function(e){
         const btn = e.target;
-
-        btn.closest("tr").style.backgroundColor = 'rgb(248, 80, 80)';
+        btn.style.boxShadow='1px 1px 0px 0px black';
+    })
+    attendedButton[i].addEventListener('mouseout', function(e){
+        const btn = e.target;
+        btn.style.boxShadow='-1px -1px 0px 0px black';
+    })
+    attendedButton[i].addEventListener('mouseup', function(e){
+        const btn = e.target;
+        btn.style.boxShadow='1px 1px 0px 0px black';
+        btn.closest("tr").style.backgroundColor = 'rgb(92, 209, 110)';
         btn.closest("tr").cells[4].innerHTML='';
         btn.closest("tr").cells[5].innerHTML='';
 
-        attendedCount.innerHTML=Number(attendedCount.innerHTML)+1
+        // added attendance condition to show in excel.
+        btn.closest("tr").cells[3].innerHTML+='Atended';
+
+        attendedButtonCount.innerHTML=Number(attendedButtonCount.innerHTML)+1
         this.disabled = true;
     })
 } 
 }
-att();
 
-//absent button
+//absentButton button (for each student)
 function abs(){
-    for(var i = 0; i<absent.length;i++){
-    absent[i].addEventListener('click', function(e){
+    for(var i = 0; i<absentButton.length;i++){
+    absentButton[i].addEventListener('mouseover', function(e){
         const btn = e.target;
-
-        btn.closest("tr").style.backgroundColor = 'rgb(2, 80, 80)';
+        btn.style.boxShadow='1px 1px 0px 0px black';
+    })
+    absentButton[i].addEventListener('mouseout', function(e){
+        const btn = e.target;
+        btn.style.boxShadow='-1px -1px 0px 0px black';
+    })
+    absentButton[i].addEventListener('click', function(e){
+        const btn = e.target;
+        btn.closest("tr").style.backgroundColor = 'rgb(236, 138, 138)';
         btn.closest("tr").cells[3].innerHTML='';
         btn.closest("tr").cells[5].innerHTML='';
+        btn.style.boxShadow='1px 1px 0px 0px black';
+        // added attendance condition to show in excel.
+        btn.closest("tr").cells[4].innerHTML+='Absent';
 
         this.disabled = true;
-        absentCount.innerHTML=Number(absentCount.innerHTML)+1
+        absentButtonCount.innerHTML=Number(absentButtonCount.innerHTML)+1
     })
 }
 }
-abs();
 
-//late button
+//lateButton button (for each student)
 function lat(){
-    for(var i = 0; i<late.length;i++){
-    late[i].addEventListener('click', function(e){
+    for(var i = 0; i<lateButton.length;i++){
+    lateButton[i].addEventListener('mouseover', function(e){
         const btn = e.target;
+        btn.style.boxShadow='1px 1px 0px 0px black';
+    })
 
-        btn.closest("tr").style.backgroundColor = 'rgb(100, 80, 80)';
+    lateButton[i].addEventListener('mouseout', function(e){
+        const btn = e.target;
+        btn.style.boxShadow='-1px -1px 0px 0px black';
+    })
+        
+    lateButton[i].addEventListener('mouseup', function(e){
+        const btn = e.target;
+        btn.style.boxShadow='1px 1px 0px 0px black';
+        btn.closest("tr").style.backgroundColor = 'rgb(145, 147, 146)';
         btn.closest("tr").cells[3].innerHTML='';
         btn.closest("tr").cells[4].innerHTML='';
+
+        // added attendance condition to show in excel.
+        btn.closest("tr").cells[5].innerHTML+='Late';
         
 
         this.disabled = true;
-        lateCount.innerHTML=Number(lateCount.innerHTML)+1
+        lateButtonCount.innerHTML=Number(lateButtonCount.innerHTML)+1
     })
 }
 }
-lat();
 
-
-clear.addEventListener('click',function(){
-    var table = document.getElementById("attendTableBody");
-    
+//clear all button
+function clr(){
+    clearButton.addEventListener('click',function(){
     for (var i = 0, row; row = table.rows[i]; i++) {
         row.style.backgroundColor = 'white';
-        row.cells[3].innerHTML = '<input type="button" class="formButton attendedButton" name="attended" id="attendedButton">'
-        row.cells[4].innerHTML = '<input type="button" class="formButton absentButton" name="absent" id="absentButton">'
-        row.cells[5].innerHTML = '<input type="button" class="formButton lateButton" name="late" id="lateButton">' 
+        row.cells[3].innerHTML = '<input type="button" class="formButton attendedButton" name="attendedButton" id="attendedButton">'
+        row.cells[4].innerHTML = '<input type="button" class="formButton absentButton" name="absentButton" id="absentButton">'
+        row.cells[5].innerHTML = '<input type="button" class="formButton lateButton" name="lateButton" id="lateButton">' 
     }
-    attendedCount.innerHTML = 0;
-    absentCount.innerHTML = 0;
-    lateCount.innerHTML = 0;
+    attendedButtonCount.innerHTML = 0;
+    absentButtonCount.innerHTML = 0;
+    lateButtonCount.innerHTML = 0;
+    reset();
+    })
+}
+
+
+//save button (as excel). // used table2excel plugin.
+saveButton.addEventListener('click', function(e){
+    var table2excel = new Table2Excel();
+    table2excel.export(document.getElementById("attendTable"));
+    window.onbeforeunload = function() {
+        return '';
+    }
+})
+
+
+var i = 2;
+//add button (inside popup)
+function addStudentForm(){
+    addForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        
+        const id = document.getElementById('id');
+        const fname = document.getElementById('fname');
+        const lname = document.getElementById('lname');
+        i++;
+        if (!id.value || !fname.value || !lname.value ){
+            alert('Fill all blanks');
+        }
+        else{
+            table.innerHTML += `
+        <tr>
+            <td class='entry'>${i}</td>
+            <td class='entry'>${id.value}</td>
+            <td class='entry'>${fname.value} ${lname.value}</td>
+            <td>
+                <input type="button" class="formButton attendedButton" name="attendedButton" id="attendedButton">
+            </td>
+            <td>
+                <input type="button" class="formButton absentButton" name="absentButton" id="absentButton">
+            </td>
+            <td>
+                <input type="button" class="formButton lateButton" name="lateButton" id="lateButton">
+            </td>
+        </tr>
+        `
+        addForm.reset();
+        reset();
+        clearButton.click();
+        popupDialog.style.display = 'none';
+        } 
+    })
+}
+
+// cancel button to discard popup (inside popup)
+function cancelFunc(){
+    cancelButton.addEventListener('click',function(){
+        console.log('c')
+        popupDialog.style.display = 'none';
+    }) 
+}
+
+// add student button (popup initiator)
+function addStudent(){
+    addStudentButton.addEventListener('click', function(){
+        popupDialog.style.display = 'flex';
+    }) 
+}
+
+//reset buttons inside form after clearing all
+function reset(){
     att();
     abs();
     lat();
-})
+}
 
-//save as excel.
-save.addEventListener('click', function(){
-    var table2excel = new Table2Excel();
-    table2excel.export(document.getElementById("attendTable"));
-})
+//functions reference
+clr();
+att();
+abs();
+lat();
+addStudentForm();
+cancelFunc();
+addStudent();
